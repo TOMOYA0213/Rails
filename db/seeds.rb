1600f3d9f1f82b628d2ff7a006ec636836c3838b
yoshidatomoya <yoshidatomoya@akane.waseda.jp>
2024-11-08 14:58:21 +0900

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
+# ユーザー
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true,
+             admin:     true,
              activated: true,
              activated_at: Time.zone.now)
 
@@ -15,11 +16,20 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password,
                activated: true,
-              activated_at: Time.zone.now)
+               activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::Lorem.sentence
+  content = Faker::Lorem.sentence      ##############(5)を削除すると解決。
   users.each { |user| user.microposts.create!(content: content) }
-end             
+end
+
+# リレーションシップ
+users = User.all #ユーザー1が、
+user  = users.first
+following = users[2..50] #3〜51をフォローする。
+followers = users[3..40] #4〜41にフォローされる。
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
